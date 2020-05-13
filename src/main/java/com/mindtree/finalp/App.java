934d6f72ca2a8e7bbd166b27028645cb2b864232
Varinder5997 <varinderbhatti3737@gmail.com>
2020-05-13 20:18:41 +0530

    First commit

---
+++ src/main/java/com/mindtree/finalp/App.java
@@ -0,0 +1,14 @@
+package com.mindtree.finalp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        return "NewFile";
+    }
+}
